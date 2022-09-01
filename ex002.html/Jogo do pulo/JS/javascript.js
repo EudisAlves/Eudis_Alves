6257fad0bb55a604ab08deff7e10a617fe8057de
EudisAlves <111400440+EudisAlves@users.noreply.github.com>
2022-09-01 00:26:23 -0300

    Update javascript.js

--- ex002.html/Jogo do pulo/JS/javascript.js
+++ ex002.html/Jogo do pulo/JS/javascript.js
@@ -1,10 +1,10 @@
 const mario = document.querySelector('.mario');
 
 const jump = () => {
-    mario.classList.add('jump');
+    mario.classList.add('.jump');
 
     setTimeout(() => {
-        mario.classList.remove('jump');
+        mario.classList.remove('.jump');
     }, 800);
 }
 
