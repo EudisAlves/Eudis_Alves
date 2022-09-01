d9ee03c83d337d4f31efae6d96488a36e14ef362
EudisAlves <111400440+EudisAlves@users.noreply.github.com>
2022-09-01 00:03:11 -0300

    Eudis_Alves

---
+++ ex002.html/Jogo do pulo/JS/javascript.js
@@ -0,0 +1,12 @@
+const mario + document.querySelector('.mario');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 800);
+}
+
+
+document.addEventListener('keydown', jump);
