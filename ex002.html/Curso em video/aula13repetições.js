f22c7ab4c5300bcde27a3d6518c437b764a9288c
EudisAlves <111400440+EudisAlves@users.noreply.github.com>
2022-09-07 10:58:45 -0300

    Create aula13repetições.js

---
+++ ex002.html/Curso em video/aula13repetições.js
@@ -0,0 +1,12 @@
+/*var c = 1
+while (c <= 6) { //enquanto c for menor ou igual à 6 ele vai repetir a funçõa
+    console.log('tudo bem')
+    c++ // ou atingir o numero de 6 ele segue o fluxo lógico
+}*/
+
+var c = 1
+do { // esse comando vai executar primeiro o bloco e depois ele faz o teste e vai repetir ate o teste for verdadeiro
+    console.log(`passo ${c}`)
+    c++
+} while ( c <= 10)
+
