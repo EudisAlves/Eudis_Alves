09123be79e8a782756894244125bf246503a280e
EudisAlves <111400440+EudisAlves@users.noreply.github.com>
2022-09-07 11:28:32 -0300

    Eudis_Alves

---
+++ ex002.html/Curso em video/aula14repetiçõespart2.js
@@ -0,0 +1,7 @@
+console.log('Vai começar...')
+
+for (var c = 1; c <= 5; c++) { // no modo for ele tem como objetivo executar o comando dentro de uma função só, que seria (inicio; teste lógico; e incrementação)
+    console.log(c)
+}
+
+console.log('Fim!')
