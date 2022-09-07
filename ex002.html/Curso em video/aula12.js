77f1e27b5ec4e3c58d5240f67f5553d315478d53
EudisAlves <111400440+EudisAlves@users.noreply.github.com>
2022-09-06 22:29:22 -0300

    Update aula12.js

--- ex002.html/Curso em video/aula12.js
+++ ex002.html/Curso em video/aula12.js
@@ -1,9 +1,10 @@
-var idade = 13
-console.log('você tem ${idade} anos.')
-if (idade < 16) {
-    console.log('não vota')
-} else if (idade < 18 || idade > 65){
-console.log('voto opcional')
-} else {
-    console.log('voto obrigatório')
+var idade = 188
+console.log(`Você tem ${idade} anos.`)
+   if (idade < 16) {
+        console.log('não vota')
+    } else if (idade < 18 || idade > 65){ //idade maio que 65 e menor que 18 voto opcional
+
+        console.log('voto opcional')
+    } else if (idade > 18 || idade > 65){ // idade maior que 18 e menor que 66 voto obrigatório
+        console.log('voto obrigatório')
 }
