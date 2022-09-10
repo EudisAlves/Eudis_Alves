a49da676bc55b19515630eecc0cd96c3aadd3cc5
EudisAlves <111400440+EudisAlves@users.noreply.github.com>
2022-09-09 23:00:14 -0300

    Update aula15variaveiscompostas.js

--- ex002.html/Curso em video/aula15variaveiscompostas.js
+++ ex002.html/Curso em video/aula15variaveiscompostas.js
@@ -2,8 +2,8 @@ let num = [ 5, 8, 2, 9, 3]
 num.push(1) /* esse código adiciona o valor na ultima vaga do vetor*/
 num.sort() /* esse código ordena de forma crescente o valores*/
 console.log(num)
-console.log(`ò vetor tem ${num.length} posições`)
-let pos = num.indexOf(8)
+console.log(`ò vetor tem ${num.length} posições`) /* o .length serve para eu saber a quantidade de valores no vetor*/
+let pos = num.indexOf(8) /* esse código é para buscar um valor no vetor*/
 if (pos ==-1) {
     console.log('O valor nao foi encontrado!')
 } else {
