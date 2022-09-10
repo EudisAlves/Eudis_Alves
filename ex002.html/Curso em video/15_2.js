d85c7bb35d01199ef180242db978f566e3e34626
EudisAlves <111400440+EudisAlves@users.noreply.github.com>
2022-09-09 22:34:49 -0300

    Update 15_2.js

--- ex002.html/Curso em video/15_2.js
+++ ex002.html/Curso em video/15_2.js
@@ -2,6 +2,6 @@ let valores = [8, 1, 7, 4, 2, 9]
 
 console.log(valores)
 
-for(let pos=o; pos < valores.length; pos++) {
+for(let pos=0; pos < valores.length; pos++) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-}
+}/* essa forma eu posso armazenar mais de um valor na mesma variate ou vetor*/
