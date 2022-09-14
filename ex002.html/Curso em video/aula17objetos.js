df56b7dc7df1222512352e0a78ec0b6fd2278045
Eudis Alves <deleudis@gmail.com>
2022-09-13 22:31:27 -0300

    Update aula17objetos.js

--- ex002.html/Curso em video/aula17objetos.js
+++ ex002.html/Curso em video/aula17objetos.js
@@ -1,10 +1,11 @@
 let amigo = {nome:'josé', 
 sexo:'M', 
 peso: 85.4, 
-engordou(p=0){
+engordar(p=0){
     console.log('engordou')
     this.peso += p
 }}
 
 amigo.engordar(2)
 console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
+/*nesse caso, vemos que a variavel, recebe diversos atributos, onde podemos ter assesso a eles de forma idenpendente*/ 
