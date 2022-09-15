19baf8269048cbc1053a869b9a2548a57886ef4b
Eudis Alves <deleudis@gmail.com>
2022-09-14 22:07:02 -0300

    Eudis_Alves

--- ex002.html/Curso em video/aula17objetos.js
+++ ex002.html/Curso em video/aula17objetos.js
@@ -1,11 +1,12 @@
 let amigo = {nome:'josé', 
 sexo:'M', 
 peso: 85.4, 
-engordar(p=0){
+engordar(p=0)
     console.log('engordou')
     this.peso += p
+
 }}
 
-amigo.engordar(2)
+amigo.engordar(-8)
 console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
 /*nesse caso, vemos que a variavel, recebe diversos atributos, onde podemos ter assesso a eles de forma idenpendente*/ 
--- ex002.html/Jogo do pulo/JS/javascript.js
+++ ex002.html/Jogo do pulo/JS/javascript.js
@@ -1,12 +1,31 @@
-const mario = document.querySelector('.mario')
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector(".pipe");
+const score = document.querySelector(".score");
+let aleardyjump = false;
+let count = 0;
 
-const jump = () => {
-    mario.classList.add('jump');
-
-    setTimeout((event) => {
-        if (event.key == "ArrowUp") {
-            mario.classList.remove('jump')
+document.addEventListener('keydown', (e)=>{
+    if((e.code ==="ArrowUp")|(e.code ==="Space")){
+        jump();
+    }
+    function jump(){
+        if(!mario.classList.contains("jump")){
+            mario.classList.add("jump");
+            aleardyjump = true;
+        setTimeout(() =>{
+            mario.classList.remove("jump");
+            aleardyjump = false;
+        }, 900);
         }
-    }, 900) 
-}
-document.addEventListener('keydown', jump)               
+    }
+    setInterval(() =>{
+        let marioBottom = parseInt(
+            window.getComputedStyle(mario).getPropertyValue("bottom"));
+        let pipeLeft = parseInt(
+            !alreadyjump){
+                alert(`Gmae Over! Seu score foi: ${count}`);
+                count = 0;
+            }    
+         count++;
+            score.innerHTML = `SCORE: ${COUNT}`;
+}, 40);              
