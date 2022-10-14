6f23e2afa6bf1a21e824d9b89eeca6bebe5ca217
Eudis Alves <deleudis@gmail.com>
2022-10-14 20:10:14 -0300

    Update javascript.js

--- ex002.html/Jogo do pulo/JS/javascript.js
+++ ex002.html/Jogo do pulo/JS/javascript.js
@@ -1,31 +1,20 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector(".pipe");
-const score = document.querySelector(".score");
-let aleardyjump = false;
-let count = 0;
+const mario = document.querySelector(" .mario");
+const pipe = document.querySelector(" .pipe");
 
-document.addEventListener('keydown', (e)=>{
-    if((e.code ==="ArrowUp")|(e.code ==="Space")){
-        jump();
-    }
-    function jump(){
-        if(!mario.classList.contains("jump")){
-            mario.classList.add("jump");
-            aleardyjump = true;
-        setTimeout(() =>{
-            mario.classList.remove("jump");
-            aleardyjump = false;
-        }, 900);
-        }
-    }
-    setInterval(() =>{
-        let marioBottom = parseInt(
-            window.getComputedStyle(mario).getPropertyValue("bottom"));
-        let pipeLeft = parseInt(
-            !alreadyjump){
-                alert(`Gmae Over! Seu score foi: ${count}`);
-                count = 0;
-            }    
-         count++;
-            score.innerHTML = `SCORE: ${COUNT}`;
-}, 40);              
+const jump = () => {
+  mario.classList.add("jump");
+
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 900);
+};
+
+const loop = setTimeout(() => {
+  const pipeposition = pipe.offsetleft;
+  if (pipeposition <= 120) {
+    pipe.style.animation = "none;";
+    pipe.style.left = `${pipeposition}px`;
+  }
+});
+
+document.addEventListener("keydown", jump);
