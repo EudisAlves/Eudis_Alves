a845f0bff512235988ca4b03799d2de19a9821ff
Eudis Alves <deleudis@gmail.com>
2022-10-15 22:05:34 -0300

    Eudis_Alves

---
+++ jogo-da-velha/javascript.js
@@ -0,0 +1,101 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text"
+);
+const restartButton = document.querySelector("[data-restart-button");
+
+const winningMessage = document.querySelector("[data-winning-message]");
+
+let isCircleTurn;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const startGame = () => {
+  for (const cell of cellElements) {
+    cell.classList.remove("circle");
+    cell.classList.remove("x");
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  }
+
+  isCircleTurn = false;
+
+  board.classList.add("x");
+  winningMessage.classList.remove("show-winning-message");
+};
+//linha de comando de vitoria e empate
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerText = "Empate!";
+  } else {
+    winningMessageTextElement.innerText = isCircleTurn
+      ? "Circulo Venceu!"
+      : "X Venceu!";
+  }
+
+  winningMessage.classList.add("show-winning-message");
+};
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
+};
+
+const checkForDraw = () => {
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains("x") || cell.classList.contains("circle");
+  });
+};
+
+const placeMark = (cell, classToAdd) => {
+  cell.classList.add(classToAdd);
+};
+
+const swapTurne = () => {
+  isCircleTurn = !isCircleTurn;
+
+  board.classList.remove("circle");
+  board.classList.remove("X");
+
+  if (isCircleTurn) {
+    board.classList.add("circle");
+  } else {
+    board.classList.add("x");
+  }
+};
+
+const handleClick = (e) => {
+  const cell = e.target;
+  const classToAdd = isCircleTurn ? "circle" : "x";
+
+  placeMark(cell, classToAdd);
+
+  const isWin = checkForWin(classToAdd);
+
+  const isDraw = checkForDraw();
+
+  if (isWin) {
+    endGame(false);
+  } else if (isDraw) {
+    endGame(true);
+  } else {
+    //mudar simbulo
+    swapTurne();
+  }
+};
+
+startGame();
+restartButton.addEventListener("click", startGame);
