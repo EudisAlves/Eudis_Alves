fedb955cf9ae5545924791dcdbd6246b645ed800
Eudis Alves <deleudis@gmail.com>
2022-10-17 20:26:23 -0300

    Update index.js

--- iniciando-no-canvas/index.js
+++ iniciando-no-canvas/index.js
@@ -12,7 +12,26 @@ context.lineWidth = 3; // espessura da borda
 context.strokeStyle = "blue"; // cor da borda
 context.strokeRect(50, 50, 100, 100);
 
-//Iniciando um novo caminho
-
 context.fillStyle(); //Preencher toda área desenhada
 context.strokeStyle(); // Contornar a área desenhada
+
+//Iniciando um novo caminho
+
+context.beginPath();
+
+// desenhando uma estrela
+
+context.moveTO(75, 250);
+context.lineTo(150, 50);
+context.lineTo(225, 250);
+context.lineTo(50, 120);
+context.lineTo(250, 120);
+context.lineTo(75, 250);
+
+//configurando a linha
+context.lineWidth = 2;
+context.strokeStyle = "red";
+
+//traçar as linhas do caminho
+
+context.strokeStyle();
