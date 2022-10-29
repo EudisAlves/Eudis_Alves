def9ad17cd80815d2639f2a4ea340ddc964767ba
Eudis Alves <deleudis@gmail.com>
2022-10-29 20:44:43 -0300

    limitando top e botton

--- estudoGit/jogo-de-nave/js/js.js
+++ estudoGit/jogo-de-nave/js/js.js
@@ -48,11 +48,17 @@ function start() {
     if (jogo.pressionou[TECLA.W]) {
       var topo = parseInt($("#jogador").css("top"));
       $("#jogador").css("top", topo - 10);
+      if (topo <= 0) { /* para limitar o elemento na tela*/
+        $("#jogador").css("top", topo + 10);
+      }
     }
 
     if (jogo.pressionou[TECLA.S]) {
       var topo = parseInt($("#jogador").css("top"));
       $("#jogador").css("top", topo + 10);
+      if (topo >= 434) { /* para nao deixar o elemento passar do botton*/
+        $("#jogador").css("top", topo - 10);
+      }
     }
 
     if (jogo.pressionou[TECLA.D]) {
