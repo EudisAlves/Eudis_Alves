9478f24b2d9040ab5991dbd8510290424906dd7f
Eudis Alves <deleudis@gmail.com>
2022-10-29 20:30:34 -0300

    Update js.js

--- estudoGit/jogo-de-nave/js/js.js
+++ estudoGit/jogo-de-nave/js/js.js
@@ -7,4 +7,56 @@ function start() {
   $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
   $("#fundoGame").append("<div id='inimigo2'></div>");
   $("#fundoGame").append("<div id='amigo'class='anima3'></div>");
+
+  //Principais variáveis do jogo
+
+  var jogo = {};
+  var TECLA = {
+    W: 87,
+    S: 83,
+    D: 68,
+  };
+  jogo.pressionou = [];
+
+  //Verifica se o usuário pressionou alguma tecla
+
+  $(document).keydown(function (e) {
+    jogo.pressionou[e.which] = true;
+  });
+
+  $(document).keyup(function (e) {
+    jogo.pressionou[e.which] = false;
+  });
+
+  //Game Loop
+
+  jogo.timer = setInterval(loop, 30); /* o loopse repetirar a cara 30ms*/
+
+  function loop() {
+    movefundo();
+    movejogador();
+  } // Fim da função loop()
+
+  //Função que movimenta o fundo do jogo
+
+  function movefundo() {
+    esquerda = parseInt($("#fundoGame").css("background-position"));
+    $("#fundoGame").css("background-position", esquerda - 1);
+  } // fim da função movefundo()
+
+  function movejogador() {
+    if (jogo.pressionou[TECLA.W]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top", topo - 10);
+    }
+
+    if (jogo.pressionou[TECLA.S]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top", topo + 10);
+    }
+
+    if (jogo.pressionou[TECLA.D]) {
+      //Chama função Disparo
+    }
+  } // fim da função movejogador()
 } // Fim da função start
