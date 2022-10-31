327efeb725a8b2991b41c1dae019ffa3b9008d79
Eudis Alves <deleudis@gmail.com>
2022-10-31 18:52:27 -0300

    mais movimentações

--- estudoGit/jogo-de-nave/js/js.js
+++ estudoGit/jogo-de-nave/js/js.js
@@ -37,6 +37,8 @@ function start() {
 
   function loop() {
     moveinimigo1();
+    moveinimigo2();
+    moveamigo();
     movefundo();
     movejogador();
     disparo();
@@ -86,6 +88,28 @@ function start() {
       $("#inimigo1").css("top", posicaoY);
     }
   } //Fim da função moveinimigo1()
+
+  function moveinimigo2() {
+    posicaoX = parseInt($("#inimigo2").css("left"));
+    $("#inimigo2").css("left", posicaoX - 3);
+
+    if (posicaoX <= 0) {
+      $("#inimigo2").css("left", 775);
+    }
+  } // Fim da função moveinimigo2()
+
+  function moveamigo() {
+	
+    posicaoX = parseInt($("#amigo").css("left"));
+    $("#amigo").css("left",posicaoX+1);
+          
+      if (posicaoX>906) {
+        
+      $("#amigo").css("left",0);
+            
+      }
+  
+  } // fim da função moveamigo()
 
   function disparo() {
     if (podeAtirar == true) {
