e65c8cfc50fba6aef6bb0d7c312e970dfafef993
Eudis Alves <deleudis@gmail.com>
2022-11-13 12:30:06 -0300

    js

---
+++ jogo-espacial/js/script.js
@@ -0,0 +1,147 @@
+(function(){
+	//canvas
+	var cnv = document.querySelector('canvas');
+	//contexto de renderização 2d
+	var ctx = cnv.getContext('2d');
+	
+	//RECURSOS DO JOGO ========================================================>
+	//arrays
+	var sprites = [];
+	var assetsToLoad = [];
+	
+	//sprites
+	//cenário
+	var background = new Sprite(0,56,400,500,0,0);
+	sprites.push(background);
+	
+	//nave
+	var defender = new Sprite(0,0,30,50,185,450);
+	sprites.push(defender);
+	
+	//imagem
+	var img = new Image();
+	img.addEventListener('load',loadHandler,false);
+	img.src = "img/img.png";
+	assetsToLoad.push(img);
+	//contador de recursos
+	var loadedAssets = 0;
+	
+	
+	//entradas
+	var LEFT = 37, RIGHT = 39, ENTER = 13, SPACE = 32;
+	
+	//direções
+	var mvLeft = mvRight = false;
+	
+	//estados do jogo
+	var LOADING = 0, PLAYING = 1, PAUSED = 2, OVER = 3;
+	var gameState = LOADING;
+	
+	//listeners
+	window.addEventListener('keydown',function(e){
+		var key = e.keyCode;
+		switch(key){
+			case LEFT:
+				mvLeft = true;
+				break;
+			case RIGHT:
+				mvRight = true;
+				break;
+		}
+	},false);
+	
+	window.addEventListener('keyup',function(e){
+		var key = e.keyCode;
+		switch(key){
+			case LEFT:
+				mvLeft = false;
+				break;
+			case RIGHT:
+				mvRight = false;
+				break;
+			case ENTER:
+				if(gameState !== PLAYING){
+					gameState = PLAYING;
+				} else {
+					gameState = PAUSED;
+				}
+		}
+	},false);
+	
+	
+	
+	//FUNÇÕES =================================================================>
+	function loadHandler(){
+		loadedAssets++;
+		if(loadedAssets === assetsToLoad.length){
+			img.removeEventListener('load',loadHandler,false);
+			//inicia o jogo
+			gameState = PAUSED;
+		}
+	}
+	
+	function loop(){
+		requestAnimationFrame(loop, cnv);
+		//define as ações com base no estado do jogo
+		switch(gameState){
+			case LOADING:
+				console.log('LOADING...');
+				break;
+			case PLAYING:
+				update();
+				break;
+		}
+		render();
+	}
+	
+	function update(){
+		//move para a esquerda
+		if(mvLeft && !mvRight){
+			defender.vx = -5;
+		}
+		
+		//move para a direita
+		if(mvRight && !mvLeft){
+			defender.vx = 5;
+		}
+		
+		//para a nave
+		if(!mvLeft && !mvRight){
+			defender.vx = 0;
+		}
+		
+		//atualiza a posição
+		defender.x = Math.max(0,Math.min(cnv.width - defender.width, defender.x + defender.vx));
+	}
+	
+	function render(){
+		ctx.clearRect(0,0,cnv.width,cnv.height);
+		if(sprites.length !== 0){
+			for(var i in sprites){
+				var spr = sprites[i];
+				ctx.drawImage(img,spr.sourceX,spr.sourceY,spr.width,spr.height,Math.floor(spr.x),Math.floor(spr.y),spr.width,spr.height);
+			}
+		}
+	}
+	
+	loop();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
