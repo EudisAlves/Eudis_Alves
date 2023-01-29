a2f3cf1620984b27fc1aed4b6c95119bcb3eeeb7
Eudis Alves <deleudis@gmail.com>
2023-01-29 09:23:34 -0300

    excluindo comentarios

--- react/cursots/src/routes/index.tsx
+++ react/cursots/src/routes/index.tsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { BrowserRouter, Route, Routes as Switch } from 'react-router-dom';
+import { Dashboard } from '../pages/dashboard/Dashboard';
 export const Routes = () => {
     return (
         <BrowserRouter>
         <Switch>
-        <Routes path="/" component={()=> <h1>Olá</h1>} />
+        <Route path="/ppagina-inicial" element={Dashboard} />
         </Switch> 
         </BrowserRouter>
     );
