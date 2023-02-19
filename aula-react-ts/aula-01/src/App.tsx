b258cf2048a0e70d155f86022f3b3043a711ccca
Eudis Alves <deleudis@gmail.com>
2023-02-19 20:45:36 -0300

    criando pastas

--- aula-react-ts/aula-01/src/App.tsx
+++ aula-react-ts/aula-01/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -11,4 +11,4 @@ function App() {
   )
 }
 
-export default App
+
