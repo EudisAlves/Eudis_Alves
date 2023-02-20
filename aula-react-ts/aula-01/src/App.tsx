fba65033fa0e917d22481f085562b23572e3f2c4
Eudis Alves <deleudis@gmail.com>
2023-02-20 08:46:30 -0300

    add bulma

--- aula-react-ts/aula-01/src/App.tsx
+++ aula-react-ts/aula-01/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import './App.css'
+import 'rbx/index.css'
 
 export default function App() {
   const [count, setCount] = useState(0)
--- aula-react-ts/aula-01/src/components/Header.tsx
+++ aula-react-ts/aula-01/src/components/Header.tsx
@@ -1,8 +1,15 @@
+
+
+
 export default function Header() {
     return (
         <div className="header">
             <h1>Olá mundo!</h1>
-            <img className="logo" src="../../public/img/ml.png" alt="" /> 
+            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
         </div>
     );
 }
