9c70ffc30784495bea1cee1336bd447d8d05362e
Eudis Alves <deleudis@gmail.com>
2023-02-19 21:24:10 -0300

    adicionando header e body

--- aula-react-ts/aula-01/src/components/Header.tsx
+++ aula-react-ts/aula-01/src/components/Header.tsx
@@ -2,6 +2,7 @@ export default function Header() {
     return (
         <div className="header">
             <h1>Olá mundo!</h1>
+            <img className="logo" src="../../public/img/ml.png" alt="" /> 
         </div>
     );
 }
