b17029b96e3a71796a81a954fc82ed4c75d14603
Eudis Alves <deleudis@gmail.com>
2023-04-10 19:40:17 -0300

    continuando slider

--- minha-web/main.js
+++ minha-web/main.js
@@ -13,5 +13,49 @@ function toggleMenu(event) {
   }
 }
 
+let slideIndex = 0;
+
+function showSlide(index) {
+  const slides = document.getElementsByClassName("slide");
+  const prev = document.getElementsByClassName("prev")[0];
+  const next = document.getElementsByClassName("next")[0];
+
+  // Verifica se chegou no primeiro ou último slide para esconder os botões prev/next
+  if (index === 0) {
+    prev.style.display = "none";
+  } else {
+    prev.style.display = "block";
+  }
+
+  if (index === slides.length - 1) {
+    next.style.display = "none";
+  } else {
+    next.style.display = "block";
+  }
+
+  // Oculta todos os slides
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // Mostra o slide atual
+  slides[index].style.display = "block";
+}
+
+function prevSlide() {
+  slideIndex--;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
+
+// Inicializa o slider com o primeiro slide
+showSlide(slideIndex);
+
+
+
 btnMenu.addEventListener('click', toggleMenu);
 btnMenu.addEventListener('touchstart', toggleMenu);
