60c2c8c156a2aa3d31c97dd1b511399d63730b88
Eudis Alves <deleudis@gmail.com>
2023-04-16 11:58:12 -0300

    Update main.js

--- climatempo/main.js
+++ climatempo/main.js
@@ -1,8 +1,15 @@
 const key = "18bd9447eefe8ff200cce9e260f5c339";
 
+async function buscarCidade(cidade) {
+    const dados = await fetch(` https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${key}`).then( resposta => resposta.json())
+
+    console.log(input)
+}
+
+
 
 function pesquisa() {
     const input = document.querySelector(".input-cidade").value
 
-    console.log(input)
+    buscarCidade(input)
 }
